cbc25bdb752d0526973162870942487083275f87
ai2playgame <detective9684@gmail.com>
2023-03-25 23:05:40 +0900

    プロジェクト用意

---
+++ SourceGens/FirstSampleGenerator/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+
+namespace FirstSampleGenerator
+{
+    [Generator(LanguageNames.CSharp)]
+    public partial class SampleGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+        }
+    }
+}
